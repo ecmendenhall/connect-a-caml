f718d7247d274ca0315930a5a49203a523a1ba72
Connor Mendenhall <ecmendenhall@gmail.com>
2013-10-12 15:31:16 -0500

    Initialize board with n * n squares.

--- Board.ml
+++ Board.ml
@@ -1,7 +1,9 @@
 open Types
 
-class gameBoard =
+class gameBoard size =
+  let initial_array = (Array.make (size * size) Empty) in
+
   object (self)
-    val squares = ( [] : square list )
+    val squares =  ( initial_array : square array)
     method getSquares = squares
   end;;
--- BoardTest.ml
+++ BoardTest.ml
@@ -1,10 +1,14 @@
 open OUnit
+open Types
 open Board
 
 let tests = "Board" >:::
   [
-    "A Board holds a list of squares" >:: ( fun () ->
-      assert_equal [] (new gameBoard)#getSquares
+    "A Board of size n holds an array of n * n squares" >:: ( fun () ->
+      assert_equal [| Empty; Empty; Empty;
+                      Empty; Empty; Empty;
+                      Empty; Empty; Empty |]
+                   (new gameBoard 3)#getSquares
     );
   ]
 
