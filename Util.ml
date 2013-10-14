8322f9c60b9145f27fa7bd32660c751b11a50d14
Connor Mendenhall <ecmendenhall@gmail.com>
2013-10-14 11:40:11 -0500

    Add basic immutable matrix operations.
    Not lists, not arrays...matrices!

--- Util.ml
+++ Util.ml
@@ -5,7 +5,7 @@ let replaceNth items index value =
 let rec splitBeforeIter n acc items = match n, items with
   | _, []     -> acc, items
   | 0, _      -> acc, items
-  | _, x::xs  -> splitBeforeIter (n - 1) (acc @ [x]) xs
+  | _, x :: xs  -> splitBeforeIter (n - 1) (acc @ [x]) xs
 
 let splitBefore n items = splitBeforeIter n [] items
 
@@ -19,8 +19,39 @@ let rec partitionByIter n acc items = match items with
 
 let partitionBy n items = partitionByIter n [] items
 
-let rec transpose items = match items with
+let rec transpose matrix = match matrix with
   | [] -> []
   | [] :: xs  -> transpose xs
   | (x :: xs) :: xss ->
       (x :: List.map List.hd xss) :: transpose (xs :: List.map List.tl xss)
+
+let makeList size default_value =
+  Array.to_list (Array.make size default_value)
+
+let squareMatrix size default_value =
+ makeList size (makeList size default_value)
+
+let valueAt i j matrix = List.nth (List.nth matrix i) j
+
+let row i matrix =  List.nth matrix i
+
+let column j matrix = List.nth (transpose matrix) j
+
+let add a b = a + b
+let sub a b = a - b
+
+let rec diagonalIter matrix acc n op = match matrix with
+  | []      -> List.rev acc
+  | x :: xs -> diagonalIter xs (List.nth x n :: acc) (op n 1) op
+
+let diagonal matrix = diagonalIter matrix [] 0 add
+
+let antidiagonal matrix = diagonalIter matrix [] (List.length matrix - 1) sub
+
+let setNth n value items =
+  let items_array = Array.of_list items in
+  items_array.(n) <- value;
+    Array.to_list items_array
+
+let setValue i j value matrix =
+  setNth i (setNth j value (row i matrix)) matrix
--- UtilTest.ml
+++ UtilTest.ml
@@ -1,5 +1,6 @@
 open OUnit
 open Util
+open Types
 
 let tests = "Util" >:::
   [
@@ -30,13 +31,100 @@ let tests = "Util" >:::
                    (partitionBy 3 [ "wha"; "pa"; "pa"; "pa"; "pa"; "pa"; "pow!" ])
     );
 
-    "transposing a list of lists" >:: (fun () ->
+    "transposing a matrix" >:: (fun () ->
       assert_equal [[1; 1; 1];
                     [0; 1; 1];
                     [0; 0; 1]]
                    (transpose [[1; 0; 0];
                                [1; 1; 0];
                                [1; 1; 1]])
+    );
+
+    "creating a square matrix" >:: (fun () ->
+      assert_equal [[0; 0; 0];
+                    [0; 0; 0];
+                    [0; 0; 0] ]
+                   (squareMatrix 3 0)
+    );
+
+    "creating a matrix filled with a default value" >:: (fun () ->
+      assert_equal [[Empty; Empty; Empty];
+                    [Empty; Empty; Empty];
+                    [Empty; Empty; Empty]]
+                   (squareMatrix 3 Empty)
+    );
+
+    "creating a list of size n filled with a default value" >:: (fun () ->
+      assert_equal [Empty; Empty; Empty; Empty]
+                   (makeList 4 Empty)
+    );
+
+    "getting an entry from the i-th row and j-th column of a matrix" >:: (fun () ->
+      let matrix = [[0; 1; 2; 3];
+                    [4; 5; 6; 7];
+                    [8; 9; 0; 5];
+                    [4; 3; 2; 1]] in
+        assert_equal 5
+                     (valueAt 2 3 matrix);
+        assert_equal 2
+                     (valueAt 0 2 matrix)
+    );
+
+    "getting the i-th row of a matrix" >:: (fun () ->
+      let matrix = [[0; 1; 2; 3];
+                    [4; 5; 6; 7];
+                    [8; 9; 0; 5];
+                    [4; 3; 2; 1]] in
+       assert_equal [4; 5; 6; 7]
+                    (row 1 matrix)
+    );
+
+    "getting the j-th column of a matrix" >:: (fun () ->
+      let matrix = [[0; 1; 2; 3];
+                    [4; 5; 6; 7];
+                    [8; 9; 0; 5];
+                    [4; 3; 2; 1]] in
+
+        assert_equal [1; 5; 9; 3]
+                     (column 1 matrix)
+    );
+
+    "getting the diagonal of a matrix" >:: (fun () ->
+      let matrix = [[0; 1; 2; 3];
+                    [4; 5; 6; 7];
+                    [8; 9; 0; 5];
+                    [4; 3; 2; 1]] in
+
+        assert_equal [0; 5; 0; 1]
+                     (diagonal matrix)
+    );
+
+    "getting the antidiagonal of a matrix" >:: (fun () ->
+      let matrix = [[0; 1; 2; 3];
+                    [4; 5; 6; 7];
+                    [8; 9; 0; 5];
+                    [4; 3; 2; 1]] in
+
+        assert_equal [3; 6; 9; 4]
+                     (antidiagonal matrix)
+    );
+
+    "'setting' the value of the i-th element in a list" >:: (fun () ->
+      assert_equal [1; 2; 3; 100]
+                   (setNth 3 100 [1; 2; 3; 4])
+    );
+
+    "'setting' the i-th row, j-th column value of a matrix" >:: (fun () ->
+      let matrix = [[0; 1; 2; 3];
+                    [4; 5; 6; 7];
+                    [8; 9; 0; 5];
+                    [4; 3; 2; 1]] in
+
+        assert_equal [[0; 1; 2; 3];
+                      [4; 5; 6; 100];
+                      [8; 9; 0; 5];
+                      [4; 3; 2; 1]]
+                     (setValue 1 3 100 matrix)
     );
   ]
 
