1baebc685c075a1f60f079b2daff7990089b5181
Connor Mendenhall <ecmendenhall@gmail.com>
2013-10-17 12:27:08 -0500

    Check in ConsoleIOFunctor tests.

---
+++ ConsoleIOFunctorTest.ml
@@ -0,0 +1,35 @@
+open OUnit
+open ConsoleIOFunctor
+open OutputFunctor
+open InputFunctor
+open Formatter
+
+let last_printed = ref ""
+
+module MockPervasives =
+  struct
+    let print_string str =
+      last_printed := str
+
+    let read_line unit =
+      "Hello world!"
+  end;;
+
+module Output = OutputFunctor (Formatter) (MockPervasives)
+module Input = InputFunctor (MockPervasives)
+module ConsoleIO = ConsoleIOFunctor (Input) (Output)
+
+let tests = "Console I/O" >:::
+  [
+    "console IO prints a prompt" >:: ( fun () ->
+      let _ = ConsoleIO.get_input in
+        assert_equal !last_printed ">> "
+    );
+
+    "console IO prints a prompt and gets input" >:: ( fun () ->
+      assert_equal "Hello world!" ConsoleIO.get_input;
+    );
+  ]
+
+(* Test Runner *)
+let _ = run_test_tt_main tests
