1191b5c19aecd92ac562b004f4d630b411263499
Connor Mendenhall <ecmendenhall@gmail.com>
2013-10-17 14:46:36 -0500

    Improve human move validation.

--- HumanFunctor.ml
+++ HumanFunctor.ml
@@ -1,4 +1,5 @@
 open IOInterface
+open Board
 open Str
 open String
 
@@ -22,14 +23,34 @@ module HumanFunctor (IO : IO) =
     let in_range size coord =
       (0 <= coord) && (coord < size)
 
-    let valid_move move board =
+    let valid_move coord board =
       let size = List.length board in
       let valid = (in_range size) in
-        match move with
+        match coord with
           | row :: col :: [] -> valid row && valid col
           | _                -> false
 
+    let row coord = List.hd coord
+
+    let col coord = List.hd (List.tl coord)
+
+    let invalid_move_error board =
+        IO.show_message "That's not a valid coordinate. Please try again." Error;
+        board
+
+    let full_square_error board =
+        IO.show_message "That square is already full. Please pick another." Error;
+        board
+
+    let make_move coord turn board =
+      if valid_move coord board then
+        try Board.fill_square (row coord) (col coord) turn board with
+          | Board.FullSquare -> full_square_error board
+      else
+        invalid_move_error board
+
     let get_move turn =
       turn_prompt turn;
+      IO.show_message "Please enter your move as a row, column coordinate." Normal;
       coord_of_string (IO.get_input ())
   end;;
--- HumanFunctorTest.ml
+++ HumanFunctorTest.ml
@@ -35,7 +35,7 @@ let tests = "Human" >:::
 
     "prompts user for a move" >:: ( fun () ->
       assert_equal [0; 0] (Human.get_move X);
-      assert_equal "Your turn, player X" !last_message
+      assert_equal "Please enter your move as a row, column coordinate." !last_message
     );
 
     "converts a nice, friendly string to a row, column coordinate" >:: ( fun () ->
@@ -46,6 +46,31 @@ let tests = "Human" >:::
       assert_equal false (Human.valid_move [10; -3] (Board.empty_board 3));
       assert_equal false (Human.valid_move [0; 3] (Board.empty_board 3));
       assert_equal true  (Human.valid_move [0; 2] (Board.empty_board 3))
+    );
+
+    "fills a square on the game board" >:: ( fun () ->
+      assert_equal [[Full X; Empty; Empty];
+                    [Empty;  Empty; Empty];
+                    [Empty;  Empty; Empty]]
+                   (Human.make_move [0; 0] X (Board.empty_board 3))
+    );
+
+    "handles trying to fill an occupied square" >:: ( fun () ->
+      assert_equal [[Full X; Empty; Empty];
+                    [Empty;  Empty; Empty];
+                    [Empty;  Empty; Empty]]
+                   (Human.make_move [0; 0] O (Board.fill_square 0 0 X (Board.empty_board 3)));
+      assert_equal "That square is already full. Please pick another."
+                   !last_message
+    );
+
+    "handles trying to fill an invalid square" >:: ( fun () ->
+      assert_equal [[Empty;  Empty; Empty];
+                    [Empty;  Empty; Empty];
+                    [Empty;  Empty; Empty]]
+                   (Human.make_move [-10; 100] O (Board.empty_board 3));
+      assert_equal "That's not a valid coordinate. Please try again."
+                   !last_message
     );
   ]
 
